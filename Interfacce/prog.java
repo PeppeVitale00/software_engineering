c666ab9307553d452dee6e83079d3668d1b243c1
PeppeVitale00 <giuseppevitale5@gmail.com>
2024-08-12 19:45:50 +0200

    Add readme file

--- Interfacce/prog.java
+++ Interfacce/prog.java
@@ -9,20 +9,3 @@ public class prog {
     }
 }
 
-/*
-Le interfacce in Java possono essere paragonate a un contratto o un accordo in cui vengono stabilite delle
- regole che devono essere seguite da chiunque voglia firmare e rispettare quell'accordo.
-
-Similitudine: Le Interfacce come un Contratto di Lavoro
-Immagina un contratto di lavoro per il ruolo di "Insegnante". Questo contratto specifica 
-che chiunque lo accetti deve svolgere determinate attività, come insegnare, preparare materiali didattici e valutare gli studenti.
- Tuttavia, il contratto non specifica esattamente come queste attività devono essere svolte; lascia i dettagli
-  dell'implementazione a chi accetta il contratto.
-
-Il contratto: L'interfaccia in Java.
-Le attività obbligatorie (come insegnare, preparare materiali, valutare): I metodi dichiarati nell'interfaccia, 
-che devono essere implementati da chi "firma" il contratto (ovvero, chi implementa l'interfaccia).
-Chi firma il contratto: Le classi che implementano l'interfaccia, come una classe InsegnanteDiMatematica o 
-InsegnanteDiStoria, che dovranno fornire le specifiche implementazioni dei metodi descritti nell'interfaccia. 
-
- */
